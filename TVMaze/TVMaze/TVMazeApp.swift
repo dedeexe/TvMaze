8660786e1a93bc1c28ee27ba431cb0bb06c24b8f
dede.exe <dede.exe@gmail.com>
2023-07-01 11:06:47 -0300

    Once upon a time...
    A small aplication that started very, very, very small, and then it
    started to grew

---
+++ TVMaze/TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by dede.exe on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
